bd5ca0e4e9ee30c1adc8f615b0ceba687dbf7d2d
GogumaYummy <sora1211x@gmail.com>
2025-09-25 06:52:13 +0900

    refactor: 불필요한 Non-null 단언 연산자 제거

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { NODE_ENV, GUILD_ID, BOT_TOKEN } from './config.js';
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
-  botGuilds: NODE_ENV === 'dev' ? [GUILD_ID!] : undefined,
+  botGuilds: NODE_ENV === 'dev' ? [GUILD_ID] : undefined,
 });
 
 client.once('clientReady', async () => {
@@ -13,4 +13,4 @@ client.once('clientReady', async () => {
   client.initApplicationCommands();
 });
 
-client.login(BOT_TOKEN!);
+await client.login(BOT_TOKEN);
