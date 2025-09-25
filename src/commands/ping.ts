fbe1fbb50a2a3dd21cd6dade9006b4f00beabca2
GogumaYummy <sora1211x@gmail.com>
2025-09-26 00:11:04 +0900

    feat: 테스트용 명령어 추가

---
+++ src/commands/ping.ts
@@ -0,0 +1,12 @@
+import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { Discord, Slash } from 'discordx';
+
+@Discord()
+export class Ping {
+  @Slash(
+    new SlashCommandBuilder().setName('핑').setDescription('봇 응답 테스트용')
+  )
+  async ping(interaction: CommandInteraction) {
+    await interaction.reply({ content: '퐁!', flags: ['Ephemeral'] });
+  }
+}
