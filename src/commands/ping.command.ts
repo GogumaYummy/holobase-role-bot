88682e9b3143a749c4d8612639f7ad18cb392268
GogumaYummy <sora1211x@gmail.com>
2025-09-26 21:52:18 +0900

    refactor: 핑 명령어 응답 개선 및 메시지 인스턴스 확인 추가

--- src/commands/ping.command.ts
+++ src/commands/ping.command.ts
@@ -1,3 +1,4 @@
+import { isMessageInstance } from '@sapphire/discord.js-utilities';
 import { Command } from '@sapphire/framework';
 import { MessageFlags } from 'discord.js';
 
@@ -10,11 +11,25 @@ export class PingCommand extends Command {
     registry.registerChatInputCommand((builder) => builder.setName('핑').setDescription('봇 동작 테스트'));
   }
 
-  public override chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-    return interaction.reply({
-      content: '퐁!',
+  public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    const callbackResponse = await interaction.reply({
+      content: '핑?',
       withResponse: true,
       flags: MessageFlags.Ephemeral,
     });
+    const message = callbackResponse.resource?.message;
+
+    if (message && isMessageInstance(message)) {
+      const difference = message.createdTimestamp - interaction.createdTimestamp;
+      const ping = Math.round(this.container.client.ws.ping);
+
+      if (ping < 0) {
+        return interaction.editReply(`아직 봇이 서버와 연결되지 않았어요 (왕복 시간: ${difference}ms)`);
+      }
+
+      return interaction.editReply(`퐁! (왕복 시간: ${difference}ms, 연결 상태: ${ping}ms)`);
+    }
+
+    return interaction.editReply('응답에 실패했어요');
   }
 }
